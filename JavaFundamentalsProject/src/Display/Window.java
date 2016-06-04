ee416321ca044119027138d99900f3dc1d7d1bb3
nikito873 <niki_genchev@abv.bg>
2016-06-05 02:13:17 +0300

    Gameloop Added

--- JavaFundamentalsProject/src/Display/Window.java
+++ JavaFundamentalsProject/src/Display/Window.java
@@ -2,15 +2,16 @@ package Display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 /**
  * Created by Rosen on 04-Jun-16.
  */
 public class Window extends Canvas implements Runnable {
 
-    public static final int WIDTH = 320;
-    public static final int HEIGHT = WIDTH / 12*9;
     public static final int SCALE = 2;
+    public static final int WIDTH = 320 * SCALE;
+    public static final int HEIGHT = WIDTH / 12*9;
     public final String TITLE = "Icy something";
 
     public boolean running = false;
@@ -40,21 +41,69 @@ public class Window extends Canvas implements Runnable {
 
     public void run() {
         while (running){
-            // insert gameloop here
+            this.requestFocus();
+            long lastTime = System.nanoTime();
+            double amountOfTicks = 60.0;
+            double ns = 1000000000 / amountOfTicks;
+            double delta = 0;
+            long timer = System.currentTimeMillis();
+            int frames = 0;
+            while (running) {
+                long now = System.nanoTime();
+                delta += (now - lastTime) / ns;
+                lastTime = now;
+
+                try {
+                    thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                while (delta >= 1) {
+                    tick();
+                    delta--;
+                }
+                if (running) {
+                    render();
+                }
+                frames++;
+                if (System.currentTimeMillis() - timer > 1000) {
+                    timer += 1000;
+                    System.out.println("FPS " + frames);
+                    frames = 0;
+                }
+            }
+            stop();
         }
     }
 
-    public void createWindow() {
-        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-        setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
-        setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+    private void tick(){
 
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        g.dispose();
+        bs.show();
+    }
+
+
+    public void createWindow() {
         JFrame frame = new JFrame(this.TITLE);
+        frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        frame.setMaximumSize(new Dimension(WIDTH, HEIGHT));
+        frame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
         frame.add(this);
-        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
+        start();
     }
 }
