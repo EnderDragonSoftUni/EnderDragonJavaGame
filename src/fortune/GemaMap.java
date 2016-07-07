02217d0b5752a12ba82d696740fbf2a9f3d2ee70
oxanahr <oxana.hristova@gmail.com>
2016-07-07 12:07:26 +0300

    Update GemaMap.java

--- src/fortune/GemaMap.java
+++ src/fortune/GemaMap.java
@@ -1,56 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package fortune;
 
-/**
- * Класс карты игры.
- * По сути, карта является многомерным числовым массивом.
- * Массив можно хранить в виде   переменной, 
- * а можно загружать из файла.
- * 
- */
+
 public abstract class GemaMap {
     
     protected int[][] map;
     
 
-    
-    /**
-     * Проверка наличия плитки на карте.
-     * @param x
-     * @param y
-     * @return 
-     */
     public boolean hasTile(int x, int y) {
        return (x >= 0 && y >= 0 && y <  getHeight() && x <  getWidth()); 
     }
-    
-    /**
-     * Получить ИД плитки.
-     * @param x
-     * @param y
-     * @return 
-     */
+ 
     public int getTileId(int x, int y) {
         return hasTile(x,y) ? map[y][x] : 0;
     }
     
-   
-    
-    /**
-     * Количество плиток на карте по высоте.
-     * @return 
-     */
+
     public int getHeight() {
         return map.length;
     }
-    
-    /**
-     * Количество плиток на карте по ширине.
-     * @return 
-     */
+
     public int getWidth() {
         return map[0].length;
     }
